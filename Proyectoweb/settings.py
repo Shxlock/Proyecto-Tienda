42d94e0852c22a292913e7cb36ee67b636bcf8e6
Juan <149827696+Shxlock@users.noreply.github.com>
2024-04-18 08:46:08 -0500

    Update settings.py

--- Proyectoweb/settings.py
+++ Proyectoweb/settings.py
@@ -145,7 +145,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "albarracinjuancarlos800@gmail.com" 
-EMAIL_HOST_PASSWORD = "qjpn vhsk bwgc rfdp"
+EMAIL_HOST_PASSWORD = ""
 
 CRISPY_ALLOWED_TEMPLATE_PACK='bootstrap4'
 CRISPY_TEMPLATE_PACK='bootstrap4'
